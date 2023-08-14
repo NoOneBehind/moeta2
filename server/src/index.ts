266ac7b3198e5edd184528e8a947dc01ce8b9bce
Hoon Won <hoon@lagood.me>
2023-08-14 20:21:44 +0900

    server - add ackCallback

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,8 @@ const io = new Server(PORT);
 io.on('connection', (socket) => {
   console.log('A client connected:', socket.id);
 
-  socket.on('message', (data) => {
+  socket.on('message', (data, ackCallback) => {
+    ackCallback();
     console.log('Received message from', socket.id, ':', data);
 
     socket.broadcast.emit('message', data);
